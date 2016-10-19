0aad2870dab9a4048516fa8e1ff075b5d7e2d1fc
Jason Downing <jhdowning@gmail.com>
2016-10-18 21:41:07 -0700

    allow for non-apikey use of the app as well as display user repos as links to the repo url

--- js/github.js
+++ js/github.js
@@ -5,22 +5,39 @@ function Repo () {
 }
 
 Repo.prototype.getUserInfo = function(user) {
-  $.get('https://api.github.com/users/' + user + '?access_token' + apiKey).then(function(response) {
-  }).fail(function(error) {
-    $('.showUser').text("There is no user matching that name");
-  });
+  if (apiKey === undefined) {
+    $.get('https://api.github.com/users/' + user).then(function(response) {
+    }).fail(function(error) {
+      $('.showUser').text("There is no user matching that name");
+    });
+  } else {
+    $.get('https://api.github.com/users/' + user + '?access_token' + apiKey).then(function(response) {
+    }).fail(function(error) {
+      $('.showUser').text("There is no user matching that name");
+    });
+  }
 };
 
 Repo.prototype.getRepos = function(user) {
-  $.get('https://api.github.com/users/' + user + '/repos?per_page=100' + '&access_token=' + apiKey).then(function(response) {
-    console.log(response);
-    var userRepos = response;
-    for (i = 0; i < userRepos.length; i++) {
-      $('.showRepos').append('<li>' + userRepos[i].name + '</li>')
-    }
-  }).fail(function(error) {
-    $('.showRepos').text(error.responseJSON.message);
-  });
+  if (apiKey === undefined) {
+    $.get('https://api.github.com/users/' + user + '/repos?per_page=100').then(function(response) {
+      var userRepos = response;
+      for (i = 0; i < userRepos.length; i++) {
+        $('.showRepos').append('<li><a href="' + userRepos[i].html_url + '">' + userRepos[i].name + '</a></li>')
+      }
+    }).fail(function(error) {
+      $('.showRepos').text(error.responseJSON.message);
+    });
+  } else {
+    $.get('https://api.github.com/users/' + user + '/repos?per_page=100' + '&access_token=' + apiKey).then(function(response) {;
+      var userRepos = response;
+      for (i = 0; i < userRepos.length; i++) {
+        $('.showRepos').append('<li><a href="' + userRepos[i].html_url + '">' + userRepos[i].name + '</a></li>')
+      }
+    }).fail(function(error) {
+      $('.showRepos').text(error.responseJSON.message);
+    });
+  }
 };
 
 exports.repoModule = Repo;
